c94eb275440a4c7b4053a791a7c03eb0c1c6045c
ModMonster <modmonster2060@gmail.com>
2026-02-13 09:21:51 -0500

    Change package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.modmonster.text_edit"
+    namespace = "ca.modmonster.textedit"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,8 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.modmonster.text_edit"
+        applicationId = "ca.modmonster.textedit"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/ca/modmonster/textedit/MainActivity.kt
+++ android/app/src/main/kotlin/ca/modmonster/textedit/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.modmonster.text_edit
+package ca.modmonster.textedit
 
 import io.flutter.embedding.android.FlutterActivity
 
